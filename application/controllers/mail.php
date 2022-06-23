89a78d5f0c4233c0956b61cc93e5f39aaa2533b1
navidaziz98 <navidaziz98@gmail.com>
2022-06-23 11:46:19 +0500

    change image with

--- application/controllers/mail.php
+++ application/controllers/mail.php
@@ -26,22 +26,19 @@ class mail extends Public_Controller
 		// 	} else {
 		// 		echo "error";
 		// 	}
-		$mail_to_send_to = "navidaziz98@gamil.com";
-		$from_email = "navidaziz99@yahoo.com";
-		$sendflag = 'send';
-		$name = 'navidaziz';
-		if ($sendflag == "send") {
-			$subject = "Message subject";
-			$email = 'navidaziz98@gamil.com';
-			$message = "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
-			//$message = $message . $_REQUEST['message'] . "\r\n"; //add message from the contact form to existing message(name of the client)
-			$headers = "From: $from_email" . "\r\n" . "Reply-To: $email";
-			$a = mail($mail_to_send_to, $subject, $message, $headers);
-			if ($a) {
-				print("Message was sent, you can send another one");
-			} else {
-				print("Message wasn't sent, please check that you have changed emails in the bottom");
-			}
-		}
+		error_reporting(-1);
+		ini_set('display_errors', 'On');
+		set_error_handler("var_dump");
+		// The message
+
+		$message = "Line 1\r\nLine 2\r\nLine 3";
+
+		// In case any of our lines are larger than 70 characters, we should use wordwrap()
+
+		$message = wordwrap($message, 70, "\r\n");
+
+		// Send
+
+		mail('navidaziz98@gmail.com', 'My Subject', $message);
 	}
 }
