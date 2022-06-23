786de03c7c0d41d37f130621d15afc6a434984b8
navidaziz98 <navidaziz98@gmail.com>
2022-06-23 11:10:23 +0500

    email

--- application/controllers/mail.php
+++ application/controllers/mail.php
@@ -10,21 +10,38 @@ class mail extends Public_Controller
 
 	public function send()
 	{
+		error_reporting();
+		// 	$this->load->library('email');
 
-		$this->load->library('email');
+		// 	$this->email->from('navidaziz98@gmail.com', 'Navid Aziz');
+		// 	$this->email->to('info@darewro.com');
+		// 	// $this->email->cc('another@another-example.com');
+		// 	// $this->email->bcc('them@their-example.com');
 
-		$this->email->from('navidaziz98@gmail.com', 'Navid Aziz');
-		$this->email->to('info@darewro.com');
-		// $this->email->cc('another@another-example.com');
-		// $this->email->bcc('them@their-example.com');
+		// 	$this->email->subject('Email Test');
+		// 	$this->email->message('Testing the email class.');
 
-		$this->email->subject('Email Test');
-		$this->email->message('Testing the email class.');
-
-		if ($this->email->send()) {
-			echo "mail send";
-		} else {
-			echo "error";
+		// 	if ($this->email->send()) {
+		// 		echo "mail send";
+		// 	} else {
+		// 		echo "error";
+		// 	}
+		$mail_to_send_to = "navidaziz98@gamil.com";
+		$from_email = "navidaziz98@gmail.com";
+		$sendflag = 'send';
+		$name = 'navidaziz';
+		if ($sendflag == "send") {
+			$subject = "Message subject";
+			$email = 'navidaziz98@gamil.com';
+			$message = "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
+			//$message = $message . $_REQUEST['message'] . "\r\n"; //add message from the contact form to existing message(name of the client)
+			$headers = "From: $from_email" . "\r\n" . "Reply-To: $email";
+			$a = mail($mail_to_send_to, $subject, $message, $headers);
+			if ($a) {
+				print("Message was sent, you can send another one");
+			} else {
+				print("Message wasn't sent, please check that you have changed emails in the bottom");
+			}
 		}
 	}
 }
